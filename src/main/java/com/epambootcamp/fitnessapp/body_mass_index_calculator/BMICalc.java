857d63a2492b06874c94fcb1618e8eb3d70f665b
flashtomi <szabo.tamas.mk@gmail.com>
2017-03-19 00:30:48 +0100

    Little changes in names

--- src/main/java/com/epambootcamp/fitnessapp/body_mass_index_calculator/BMICalc.java
+++ src/main/java/com/epambootcamp/fitnessapp/body_mass_index_calculator/BMICalc.java
@@ -1,6 +1,6 @@
 package com.epambootcamp.fitnessapp.body_mass_index_calculator;
 
-public class BMICalc{
+public class BMICalc {
 	private final float height;
 	private final float weight;
 	private HeightUnit heightUnit;
@@ -10,7 +10,7 @@ public class BMICalc{
 	public final WeightConverter weightConverter = new WeightConverter();
 	
 	
-	private BMICalc(BMICalcBuilder builder){
+	private BMICalc(Builder builder){
 		this.weight = builder.weight;
 		this.height = builder.height;
 		this.heightUnit = builder.heightUnit;
@@ -25,33 +25,51 @@ public class BMICalc{
 		return weight;
 	}
 	
-	public BMIResult calc(){
+	public HeightUnit getHeightUnit() {
+		return heightUnit;
+	}
+	
+	public WeightUnit getWeightUnit() {
+		return weightUnit;
+	}
+	
+	/** 
+	 * Calculator method that convert the input values to Meter and Kilogram, then calculate the BMI and return with the category
+	 * @return   with the BMI result from ENUM class
+	 * @throws IllegalArgumentException   if input values are invalid
+	 */
+	
+	public BMIResult calc() throws IllegalArgumentException {
+		if (this.weight <= 0) throw new IllegalArgumentException("Illegal weight value. Weight value should be greater than 0.");
+		if (this.height <= 0) throw new IllegalArgumentException("Illegal height value. Height value should be greater than 0.");
+		
 		final float heightInMeter = heightConverter.convert(height, heightUnit);
 		final float weightInMeter = weightConverter.convert(weight, weightUnit);
 		
 		final float bmi = weightInMeter / (heightInMeter*heightInMeter);
 		
-		System.out.println("Your BMI is:" + bmi + "kg/m2");
+		System.out.println("Your BMI is: " + bmi + " kg/m2");
 		return BMIResult.getFromValue(bmi);		
 	}
 	
-	public static class BMICalcBuilder{
-		private float height;
-		private float weight;	
-		private HeightUnit heightUnit;
-		private WeightUnit weightUnit;;
+	public static class Builder {
+		private final float height;
+		private final float weight;
 		
-		public BMICalcBuilder(float height, float weight){
+		private HeightUnit heightUnit = HeightUnit.CM;
+		private WeightUnit weightUnit = WeightUnit.KG;
+		
+		public Builder(float height, float weight) {
 			this.height = height;
 			this.weight = weight;
 		}
 		
-		public BMICalcBuilder setHeightUnit(HeightUnit unit){
+		public Builder setHeightUnit(HeightUnit unit) {
 			this.heightUnit = unit;
 			return this;			
 		}
 		
-		public BMICalcBuilder setWeightUnit(WeightUnit unit){
+		public Builder setWeightUnit(WeightUnit unit) {
 			this.weightUnit = unit;
 			return this;
 		}
