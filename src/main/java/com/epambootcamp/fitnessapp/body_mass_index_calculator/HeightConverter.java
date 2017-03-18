e0dd8f73a9f0b8f7a095e814d1b9815872976a13
flashtomi <szabo.tamas.mk@gmail.com>
2017-03-19 00:37:52 +0100

    Add javadoc to HeightConverter.java and WeightConverter.java

--- src/main/java/com/epambootcamp/fitnessapp/body_mass_index_calculator/HeightConverter.java
+++ src/main/java/com/epambootcamp/fitnessapp/body_mass_index_calculator/HeightConverter.java
@@ -1,6 +1,12 @@
 package com.epambootcamp.fitnessapp.body_mass_index_calculator;
 
 public class HeightConverter {
+	/**
+	 * Converter method to convert input height value to Meter 
+	 * @param height   height value from readNonZeroAndValidValue method
+	 * @param unit   input height unit from readUnit method
+	 * @return   Height in Meter
+	 */
 	public float convert(float height, HeightUnit unit){
 		return height / unit.getInMeters();
 	}
--- src/main/java/com/epambootcamp/fitnessapp/body_mass_index_calculator/WeightConverter.java
+++ src/main/java/com/epambootcamp/fitnessapp/body_mass_index_calculator/WeightConverter.java
@@ -1,6 +1,12 @@
 package com.epambootcamp.fitnessapp.body_mass_index_calculator;
 
 public class WeightConverter {
+	/**
+	 * Converter method to convert input weight value to Kilogram
+	 * @param weight   weight value from readNonZeroAndValidValue method
+	 * @param unit   input weight unit from readUnit method
+	 * @return   Weight in Kilogram
+	 */
 	public float convert(float weight, WeightUnit unit){
 		return weight / unit.getInKilos();
 	}
