719cd42ab57c3628ecb6d59af4dbf2853eef4b1d
flashtomi <szabo.tamas.mk@gmail.com>
2017-03-19 00:37:04 +0100

    Some new method to WeightUnit.java and HeightUnit.java
    - add contains and print methods
    - add javadoc

--- src/main/java/com/epambootcamp/fitnessapp/body_mass_index_calculator/HeightUnit.java
+++ src/main/java/com/epambootcamp/fitnessapp/body_mass_index_calculator/HeightUnit.java
@@ -1,19 +1,60 @@
 package com.epambootcamp.fitnessapp.body_mass_index_calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ENUM class with the available height units and conversion values to Meter
+ */
 public enum HeightUnit {
 	MM(1000),
 	CM(100),
 	M(1),
 	INCH(39.3700787f),
 	FEET(3.2808399f);
-	
+
+	/**
+	 * Check that the input height unit is available
+	 * @param s   input height unit
+	 * @return   true if available false if invalid
+	 */	
+	public boolean contains(String s) {
+		for (HeightUnit heightunit : values())
+			if (heightunit.name().equals(s.toUpperCase()))
+				return true;
+		return false;
+	}
+
+	/**
+	 * Print out valid height units
+	 */
+	public static void printHeightUnits()
+	{
+		System.out.print("Valid units: ");
+		for(HeightUnit heightunit:values())
+			System.out.print(heightunit + " ");
+		System.out.println();
+	}
+
 	private final float inMeter;
-	
-	HeightUnit(float inMeter){
+
+	HeightUnit(float inMeter) {
 		this.inMeter = inMeter;
 	}
-	
-	public float getInMeters(){
+
+	public float getInMeters() {
 		return inMeter;
+	}
+	
+	/**
+	 * Put valid height values in a List
+	 * @return   Array list with the height units
+	 */
+	public static List<String> getValidValues() {
+		List<String> validUnits = new ArrayList<String>();
+		for(HeightUnit heightUnit:values()) {
+			validUnits.add(heightUnit.name());
+		}
+		return validUnits;
 	}
 }
--- src/main/java/com/epambootcamp/fitnessapp/body_mass_index_calculator/WeightUnit.java
+++ src/main/java/com/epambootcamp/fitnessapp/body_mass_index_calculator/WeightUnit.java
@@ -1,10 +1,40 @@
 package com.epambootcamp.fitnessapp.body_mass_index_calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ENUM class with the available weight units and conversion values to Kilogram
+ */
 public enum WeightUnit {
 	G(1000),
 	DKG(100),
 	KG(1),
 	POUND(2.20462262f);
+	
+	/**
+	 * Check that the input weight unit is available
+	 * @param s   input weight unit
+	 * @return   true if available false if invalid
+	 */
+	public boolean contains(String s)
+	  {
+	      for(WeightUnit weightunit:values())
+	           if (weightunit.name().equals(s.toUpperCase())) 
+	              return true;
+	      return false;
+	  } 
+	
+	/**
+	 * Print out valid weight units
+	 */
+	public static void printWeightUnits()
+	{
+		System.out.print("Valid units: ");
+		for(WeightUnit weightunit:values())
+			System.out.print(weightunit + " ");
+		System.out.println();
+	}
 	
 	private final float inKilo;
 	
@@ -14,5 +44,17 @@ public enum WeightUnit {
 	
 	public float getInKilos(){
 		return inKilo;
+	}
+	
+	/**
+	 * Put valid weight values in a List
+	 * @return   Array list with the weight units
+	 */
+	public static List<String> getValidValues() {
+		List<String> validUnits = new ArrayList<String>();
+		for(WeightUnit weightUnit:values()) {
+			validUnits.add(weightUnit.name());
+		}
+		return validUnits;
 	}
 }
